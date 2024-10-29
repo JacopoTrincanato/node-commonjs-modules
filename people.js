5ce7db18b6ccdb464d8007b1d90b0624917a6aa5
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-29 15:40:17 +0100

    create package.json and import functions in people.js

--- people.js
+++ people.js
@@ -1,5 +1,10 @@
 /*people.js*
-Importa la tua funzione da names.js
-Importa la tua funzione da hobbies.js
+
 Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
 All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.*/
+
+//Importa la tua funzione da names.js
+const names = require('./names');
+
+//Importa la tua funzione da hobbies.js
+const hobbies = require('./hobbies');
